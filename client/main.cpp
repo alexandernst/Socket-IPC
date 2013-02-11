691e5cd5a3a5734ee6e40d96c80a8ddedd8e139a
Alexander Nestorov <alexandernst@gmail.com>
2013-02-11 12:18:07 +0100

    Update main.cpp to reflect client changes

--- client/main.cpp
+++ client/main.cpp
@@ -6,10 +6,19 @@ int main(int argc, char *argv[]){
 
     Client *client = new Client("ipc");
 
-    QString msg;
+    QObject::connect(client, &Client::newMessageFromServer, [](QString message){
+        qDebug() << "Received message from server:" << message;
+    });
+
+    QObject::connect(client, &Client::disconnected, [](){
+        qDebug() << "Disconnected from server";
+    });
+
+    QString message;
     for(int i = 0; i <= 10; i++)
-        msg.append(QChar(rand() % ((90 + 1) - 65) + 65)); //From ASCII 65 to 90 (A-Z)
-    client->sendMessage(msg);
+        message.append(QChar(rand() % ((90 + 1) - 65) + 65)); //From ASCII 65 to 90 (A-Z)
+    qDebug() << "Sending this message:" << message;
+    client->sendMessage(message);
     
     return a.exec();
 }
